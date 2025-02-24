8f4172c435c52aff447a9bd56ca812d1d3a5c515
Fil-Ma <filippo.mazzalai@gmail.com>
2025-02-24 16:50:53 +0100

    commit test

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -26,11 +26,6 @@ const ThemeProvider = ({ customTheme, children }: IThemeProvider) => {
 
 const useTheme = () => {
   const context = useContext(ThemeContext);
-
-  if (!context) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-
   return context;
 };
 
